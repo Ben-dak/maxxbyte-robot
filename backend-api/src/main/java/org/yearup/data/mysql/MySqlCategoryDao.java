03f16e4e00e5796a66c157f3a260b5bc2f4516f5
Ben-dak <dominickfrench.oe@gmail.com>
2025-12-13 14:07:32 -0600

    Added "get" and create methods to MySqlCategoryDao.

--- backend-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ backend-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,8 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -19,23 +19,90 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 
     @Override
     public List<Category> getAllCategories()
-    {
-        // get all categories
-        return null;
+    {   // get all categories
+        List<Category> categories = new ArrayList<>();
+
+        String sql = """
+            SELECT category_id, name, description
+            FROM categories
+            """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery())
+        {
+            while (resultSet.next())
+            {
+                categories.add(mapRow(resultSet));
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return categories;
     }
 
     @Override
     public Category getById(int categoryId)
     {
-        // get category by id
+        String sql = """
+            SELECT category_id, name, description
+            FROM categories WHERE category_id = ?
+            """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql))
+        {
+            preparedStatement.setInt(1, categoryId);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery())
+            {
+                if (resultSet.next())
+                {
+                    return mapRow(resultSet);
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 
     @Override
     public Category create(Category category)
     {
-        // create a new category
-        return null;
+        String sql = """
+            INSERT INTO categories (name, description)
+            VALUES (?, ?)
+            """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+        {
+            preparedStatement.setString(1, category.getName());
+            preparedStatement.setString(2, category.getDescription());
+
+            preparedStatement.executeUpdate();
+
+            try (ResultSet keys = preparedStatement.getGeneratedKeys())
+            {
+                if (keys.next())
+                {
+                    category.setCategoryId(keys.getInt(1));
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        return category;
     }
 
     @Override
